ab1eae909748d626bf65e5809f8ab0cfecce5c53
Sergio Gernyak <sergeg1990@gmail.com>
2018-02-14 11:39:55 +0200

    Introduced checking profile for commit msg hook
    The main goal is to be able to write a test and do not check the logic
    manually using some real git repo

---
+++ test/core/commit_message_rules/default_checking_profile_test.rb
@@ -0,0 +1,8 @@
+require_relative '../../test_helper'
+
+class CommitMessageRules::DefaultCheckingProfileTest < Minitest::Test
+  def test_check_runs_proper_number_of_rules
+    CommitMessageRules::RuleRunner.expects(:run).times(5)
+    CommitMessageRules::DefaultCheckingProfile.check(commit: mock())
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
+require 'mocha'
+require 'mocha/mini_test'
 
 require_relative './.env'
 require_relative '../core/git_hooks'
