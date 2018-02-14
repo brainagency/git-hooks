578e7cfd159fa01936ea91c4449a8c6a2773fc9e
Sergio Gernyak <sergeg1990@gmail.com>
2018-02-14 08:10:37 +0200

    Message description should have issue code rule
    Unless the other part has it or there is a skipping label

---
+++ core/commit_message_rules/message_description_should_have_issue_code_unless_another_part_has_it_rule.rb
@@ -0,0 +1,32 @@
+require_relative './base_rule'
+
+module CommitMessageRules
+  class MessageDescriptionShouldHaveIssueCodeUnlessAnotherPartHasItRule < BaseRule
+    def violated?
+      description_has_not_issue_code? &&
+        description_does_not_skip_issue_code? &&
+        current_branch_has_not_issue_code_in_name?
+    end
+
+    def error_message
+      <<-ERROR_MSG
+The additional lines do not contain the issue code!
+Please, make sure to mark commit with it!
+      ERROR_MSG
+    end
+
+    private
+
+    def description_has_not_issue_code?
+      commit.message_additional_lines_joined.match(ISSUE_REGEX) == nil
+    end
+
+    def description_does_not_skip_issue_code?
+      commit.message_additional_lines_joined.match(NO_ISSUE_REGEX) == nil
+    end
+
+    def current_branch_has_not_issue_code_in_name?
+      commit.branch_name.match(ISSUE_REGEX) == nil
+    end
+  end
+end
