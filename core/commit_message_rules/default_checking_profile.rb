c20f89db752928048c0a1472e27340fa20eaff9a
Sergio Gernyak <sergeg1990@gmail.com>
2018-02-19 17:55:48 +0200

    Moved commit msg checking desc to the profile class
    I think, the new place is more appropriate for that description

--- core/commit_message_rules/default_checking_profile.rb
+++ core/commit_message_rules/default_checking_profile.rb
@@ -1,4 +1,25 @@
 module CommitMessageRules
+  #
+  # Checks commit message text and its format
+  #
+  # In order to:
+  #   * make commit messages more standartized
+  #   * make commit message to tell what this particular commit provides
+  #   * make commit message more convenient for the other developers
+  # the following rules will be applied:
+  #   * main line MUST to not contain issue number.
+  #     Mostly issue number is useful for the tickets systems, CI systems and others, but not for
+  #     developers. Actually issue number takes a lot of space, which is not so big, to write
+  #     meaningful commit description, because mostly everywhere git shows only this first line.
+  #   * main line MUST has length not more than 50 characters.
+  #     Of course you can write more, but only 50 characters will be showen, for example, in the
+  #     bitbucket and other systems.
+  #   * additional lines MUST has issue number OR `[no-issue]` label
+  #     In order to allow specialized systems to link commits and issues. If you do not want that,
+  #     just add `[no-issue]` label.
+  #   * additional lines MUST has more detailed description about what this commit carries inside
+  #     For example: what was the problem and what is a solution, what has been improved and so on.
+  #
   class DefaultCheckingProfile
     RULE_CLASSES = [
       MessageMainLineShouldNotHaveIssueNumberRule,
