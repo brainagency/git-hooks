04ff27d47a9db8665bacc4015ffbdcab4fa1c662
Sergio Gernyak <sergeg1990@gmail.com>
2018-02-19 17:31:01 +0200

    Introduced NO_VIOLATION constant

--- core/commit_message_rules/rule_check_result.rb
+++ core/commit_message_rules/rule_check_result.rb
@@ -1,5 +1,7 @@
 module CommitMessageRules
   class RuleCheckResult
+    NO_VIOLATION = :no_violation
+
     def initialize(is_violated:, violation_code:, exit_if_violated: false, message: '')
       @violated = is_violated
       @exit_if_violated = exit_if_violated
@@ -8,7 +10,7 @@ module CommitMessageRules
     end
 
     def self.build_not_violated
-      @_not_violated_result ||= new(is_violated: false, violation_code: :no_violation)
+      @_not_violated_result ||= new(is_violated: false, violation_code: NO_VIOLATION)
     end
 
     def violated?; @violated; end
