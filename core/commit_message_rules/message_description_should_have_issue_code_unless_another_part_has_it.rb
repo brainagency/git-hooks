e3464a04b74aad7fd9c66c827d73eb5afb804eb2
Sergio Gernyak <sergeg1990@gmail.com>
2018-02-19 23:20:24 +0200

    Refactored rules which could be skipped
    Made skip step more explicit

--- core/commit_message_rules/message_description_should_have_issue_code_unless_another_part_has_it.rb
+++ core/commit_message_rules/message_description_should_have_issue_code_unless_another_part_has_it.rb
@@ -3,9 +3,8 @@ require_relative './base_rule'
 module CommitMessageRules
   class MessageDescriptionShouldHaveIssueCodeUnlessAnotherPartHasIt < BaseRule
     def violated?
-      description_has_not_issue_code? &&
-        description_does_not_skip_issue_code? &&
-        current_branch_has_not_issue_code_in_name?
+      return false if skipped?
+      description_has_not_issue_code? && current_branch_has_not_issue_code_in_name?
     end
 
     def error_message
@@ -17,13 +16,18 @@ Please, make sure to mark commit with it!
 
     private
 
+    def skipped?
+      description_has_skip_issue_code_label?
+    end
+
+    def description_has_skip_issue_code_label?
+      commit.message_description.match(NO_ISSUE_REGEX) != nil
+    end
+
     def description_has_not_issue_code?
       commit.message_description.match(ISSUE_REGEX) == nil
     end
 
-    def description_does_not_skip_issue_code?
-      commit.message_description.match(NO_ISSUE_REGEX) == nil
-    end
 
     def current_branch_has_not_issue_code_in_name?
       commit.branch_name.match(ISSUE_REGEX) == nil
