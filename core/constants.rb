678cad2edbb6f813948830eb15e605193a72b4b6
Sergio Gernyak <sergeg1990@gmail.com>
2018-03-13 14:50:57 +0200

    Dirty implementation for just saving wrong msg
    Issue #8

--- core/constants.rb
+++ core/constants.rb
@@ -3,11 +3,13 @@ module Constants
   # For example:
   #   * in Jira each project has a code.
   #     So an issue code in a particular project will be `<PROJECT-CODE>-<ISSUE-NUMBER>`.
-  ISSUE_REGEX=/#{GIT_HOOKS_PROJECT_CODE}-\d*/
-  NO_ISSUE_REGEX=/\[no-issue\]/
-  NO_DESCRIPTION_REGEX=/\[no-description\]|\[no-desc\]/
-  COMMENT_LINE_REGEX=/^\#.*/
-  MAIN_LINE_MAX_LENGTH=50
-  LENGTH_OF_DESCRIPTION_THRESHOLD=2
+  ISSUE_REGEX = /#{GIT_HOOKS_PROJECT_CODE}-\d*/
+  NO_ISSUE_REGEX = /\[no-issue\]/
+  NO_DESCRIPTION_REGEX = /\[no-description\]|\[no-desc\]/
+  COMMENT_LINE_REGEX = /^\#.*/
+  MAIN_LINE_MAX_LENGTH = 50
+  LENGTH_OF_DESCRIPTION_THRESHOLD = 2
+  PROJECT_ROOT_PATH = File.expand_path(__FILE__).gsub(/(\.git.*)/, '')
+  WRONG_COMMIT_MSG_FILE_NAME = 'COMMIT_EDITMSG_WRONG'.freeze
 end
 
